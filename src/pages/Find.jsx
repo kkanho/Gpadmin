f884f14322cc351349bcbca8e0fc38c301bed88f
kan <kanho2003@gmail.com>
2023-03-03 21:09:43 +0800

    Decoration of navbar, filter the User?, start building the profile page

--- src/pages/Find.jsx
+++ src/pages/Find.jsx
@@ -2,108 +2,114 @@ import { Button, Container } from "react-bootstrap"
 import { useContext } from "react"
 import { AuthContext } from "../context/AuthContext"
 import React, { useState, useEffect } from 'react'
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, getDoc, doc } from "firebase/firestore";
 import { db } from "../data/firebase"
 
-export default function Find() { 
-    console.log("================================================================")
 
-    const [data, setData] = useState([]) // database
+export default function Find() {
 
-    //use getDocs just for getting data for once
+    const [filteredUser, setFilteredUser] = useState([]) // cgpa filtered list
+
+    //use getDocs just for getting all the data(including current user) for once
     useEffect(() => {
         const fetchData = async () => {
-            let list = []
-            try{
+            let users = []
+            try {
                 const querySnapshot = await getDocs(collection(db, "users"));
                 //loop through all the users
                 querySnapshot.forEach((doc) => {
-                    list.push({id: doc.id, ...doc.data()})
+                    users.push({ id: doc.id, ...doc.data() })
                 });
-                console.log("list: ", list)
-                setData(list)
-            }catch(err) {
+                console.log(users)
+                //get current user data by their uid
+                const docRef = doc(db, "users", currentUser.uid);
+                getDoc(docRef)
+                    .then((doc) => {
+                        const current = doc.data()
+                        console.log(doc.data())
+                        return current
+                    })
+                    //start filter the user
+                    .then((current) => {
+                        setFilteredUser(users
+                            .filter((user) => { //remove the currentUser form the data
+                                return user.id !== currentUser.uid
+                            })
+                            .filter((user) => {
+                                return user.CGPA <= current.CGPA + 0.8 && user.CGPA >= current.CGPA - 0.8
+                            })
+                            // .filter((user) => {
+                            //     return 
+                            // })
+                        )
+                    })
+            } catch (err) {
                 console.log(err)
             }
         }
         fetchData()
     }, [])
-    console.log("data: ", data)
+    console.log("data: ", filteredUser)
 
     var matchedUsers = [] // subject list
-    var filteredUser = [] // cgpa filtered list
 
     const { currentUser } = useContext(AuthContext) // get current uid
-    const current = data.filter( user => user.id === currentUser.uid) // get data through uid
+    // console.log(currentUser)
+    // const current = data.filter( user => user.id === currentUser.uid) // get data through uid
 
-    function match(){
-        console.log("--------------------match() start--------------------")
-        var filtered = false
-        var matched = false
 
-        for(var i=0; i<=data.length-1; i++){
-            console.log("data[i]: ", data[i].id)
-            console.log("current[0]: ", current[0].id)
-            // for(var j=0; j<=data[i].Subject.length-1; j++){
-            //     if(data[i].Subject[j] == "SEHH" // test subject code as SEHH
-            //         && data[i].id !== current[0].id){ // filter out current user
-            //             matchedUsers.push(data[i])
-            //             matched = true
-            //             console.log("matched list: ", matchedUsers, matchedUsers.length)
-            //         }
-            // }
-        }
-        if(!matched){
-            console.log("no matched")
-        }
-        console.log("matched status:", matched) 
-        if(matched){
-            for(var i=0; i<=matchedUsers.length-1; i++){
-                console.log("--------------------filter loop", i, "start--------------------") 
-                for(var j=3; j<=30; j+=3){
-                    console.log("matching user: ",matchedUsers[i].id, matchedUsers[i].CGPA)
-                    if(matchedUsers[i].CGPA >= current[0].CGPA - j/10 // lower limit
-                        && matchedUsers[i].CGPA <= current[0].CGPA + j/10){ // upper limit
-                            filteredUser.push(matchedUsers[i])
-                            filtered = true
-                            console.log("filtered list: ", filteredUser)
-                            break
-                            }
-                    console.log("match loop: ", i, "gpa range:", j/10)
-                }     
-                console.log("--------------------filter loop", i, "end--------------------") 
-            }
-        console.log("filtered status: ", filtered)
-        console.log("--------------------match() end--------------------")
-        }
-    }
 
-    function display(){
-        console.log("--------------------display() start--------------------")
-        match()
-        if(filteredUser[0] != null) {
-            return (
-                <div>
-                    <div>Full Name: {filteredUser[0].FullName}</div>
-                    <div>User Name: {filteredUser[0].UserName}</div>
-                    <div>ID: {filteredUser[0].StudentID}</div>
-                    <div><img src={filteredUser[0].img} alt={filteredUser[0].UserName + " img prove"} style={{width: "100px", height: "100px"}}/></div>
-                    <div>CGPA: {filteredUser[0].CGPA}</div>
-                    {console.log("displaying filtered user")}
-                    {console.log("--------------------display() end--------------------")}
-                </div>
-            )
-        }
-        else {
-            return (
-                <div>
-                    <p align="center">No match found!</p>
-                    {console.log("--------------------display() end--------------------")}
-                </div>
-            )
-        }  
-    }
 
+    // function match(){
+    //     console.log("--------------------match() start--------------------")
+    //     var filtered = false
+    //     var matched = false
+
+    //     for(var i=0; i<=data.length-1; i++){
+    //         console.log("data["+i+"] id: ", data[i].id)
+    //         console.log("data["+i+"]: ", data[i])
+    //         // console.log("current[0]: ", current[0].id)
+    //         // for(var j=0; j<=data[i].Subject.length-1; j++){
+    //         //     if(data[i].Subject[j] == "SEHH" // test subject code as SEHH
+    //         //         && data[i].id !== current[0].id){ // filter out current user
+    //         //             matchedUsers.push(data[i])
+    //         //             matched = true
+    //         //             console.log("matched list: ", matchedUsers, matchedUsers.length)
+    //         //         }
+    //         // }
+    //     }
+    //     data.forEach(item => (
+    //         (item.id === current.id)? (
+    //             matched = false
+    //         ) : (
+    //             matchedUsers.push(item.id),
+    //             matched = true
+    //         )
+    //     ))
+    //     if(!matched){
+    //         console.log("no matched")
+    //     }
+    //     console.log("matched status:", matched) 
+    //     if(matched){
+    //         for(var i=0; i<=matchedUsers.length-1; i++){
+    //             console.log("--------------------filter loop", i, "start--------------------") 
+    //             for(var j=3; j<=30; j+=3){
+    //                 console.log("matching user: ",matchedUsers[i].id, matchedUsers[i].CGPA)
+    //                 if(matchedUsers[i].CGPA >= current[0].CGPA - j/10 // lower limit
+    //                     && matchedUsers[i].CGPA <= current[0].CGPA + j/10){ // upper limit
+    //                         filteredUser.push(matchedUsers[i])
+    //                         filtered = true
+    //                         console.log("filtered list: ", filteredUser)
+    //                         break
+    //                         }
+    //                 console.log("match loop: ", i, "gpa range:", j/10)
+    //             }     
+    //             console.log("--------------------filter loop", i, "end--------------------") 
+    //         }
+    //     console.log("filtered status: ", filtered)
+    //     console.log("--------------------match() end--------------------")
+    //     }
+    // }
 
     return (
         <Container className="mt-4">
@@ -112,19 +118,31 @@ export default function Find() {
             <Container className="col-xl-5 col-lg-8 col-md-10 col-12">
                 <div className="row">
                     <div className="col-3">
-                        <Button style={{ width: "100px", height: "500px",}} type="submit" value="reject">Reject</Button>
+                        <Button style={{ width: "100px", height: "500px", }} type="submit" value="reject">Reject</Button>
                     </div>
                     <div className="col-6">
                         {
-                            display()
+                            (filteredUser[0] != null) ? (
+                                <div>
+                                    <div>Full Name: {filteredUser[0].FullName}</div>
+                                    <div>User Name: {filteredUser[0].UserName}</div>
+                                    <div>ID: {filteredUser[0].StudentID}</div>
+                                    <div><img src={filteredUser[0].img} alt={filteredUser[0].UserName + " img prove"} style={{ width: "100px", height: "100px" }} /></div>
+                                    <div>CGPA: {filteredUser[0].CGPA}</div>
+                                </div>
+                            ) : (
+                                <div>
+                                    <p align="center">No match found!</p>
+                                </div>
+                            )
                         }
                     </div>
                     <div className="col-3">
-                        <Button style={{ width: "100px", height: "500px",}} type="submit" value="accept">Accept</Button>
+                        <Button style={{ width: "100px", height: "500px", }} type="submit" value="accept">Accept</Button>
                     </div>
                 </div>
             </Container>
         </Container>
     )
-    
+
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,135 +1,99 @@
-import React, { useState } from 'react'
+import { TextField } from '@mui/material';
+import React, { useState, useContext } from 'react'
 import { Card, Container, Form, Row, Col, InputGroup } from 'react-bootstrap'
+import { AuthContext } from '../context/AuthContext';
+import { doc, getDoc } from "firebase/firestore";
+import { db } from '../data/firebase'
 
 export default function Profile() {
 
     const [error, setError] = useState()
 
-    function handleSubmit(){
-
-    }
-
-    function handleInput(){
-        
-    }
+    const { currentUser } = useContext(AuthContext)
     
+    //get current user data by their uid
+    const docRef = doc(db, "users", currentUser.uid);
+    getDoc(docRef)
+        .then((doc) => {
+            console.log(doc.data())
+        })
+    // const docSnap = await getDoc(docRef);
+
+    // if (docSnap.exists()) {
+    // console.log("Document data:", docSnap.data());
+    // } else {
+    // // doc.data() will be undefined in this case
+    // console.log("No such document!");
+    // }
+
+    function handleSubmit() {
+
+    }
+
+    function handleInput() {
+
+    }
+
     return (
         <Container className="d-flex flex-column justify-content-center align-items" style={{ height: "90vh" }}>
             <Container className="d-flex justify-content-center">
-            <Card style={{ width: '50rem'}}>
-                <Row className="justify-content-center p-4 fs-4">Hello There!</Row>
-                <Container className="ml-4">
-                    <Form className="justify-content-center" noValidate onSubmit={handleSubmit}>
-                        <Row className="mb-2">
-                            <Form.Group as={Col} md="6" controlId="FullName">
-                                <Form.Label className="fs-5">Full Name</Form.Label>
-                                <Form.Control
-                                    disabled
-                                    required
-                                    type="text"
-                                    placeholder="Enter your full name here"
-                                    onChange={handleInput}
-                                />
-                                <Form.Control.Feedback>Looks cool!</Form.Control.Feedback>
-                            </Form.Group>
-                            <Form.Group as={Col} md="6" controlId="UserName">
-                                <Form.Label className="fs-5">User Name</Form.Label>
-                                <Form.Control
-                                    disabled
-                                    type="text"
-                                    placeholder="UserName"
-                                    required
-                                    onChange={handleInput}
-                                />
-                                <Form.Control.Feedback>
-                                    Looks cool!
-                                </Form.Control.Feedback>
-                            </Form.Group>
-                        </Row>
-                        <Row className="mb-2">
-                            <Form.Group as={Col} md="12" controlId="StudentID">
-                                <Form.Label className="fs-5">Student Email</Form.Label>
-                                <InputGroup>
-                                    <Form.Control
-                                        disabled
-                                        required
-                                        name="studentID"
-                                        onChange={handleInput}
-                                        type="text"
-                                        placeholder="Student ID"
-                                        aria-describedby="inputGroupPrepend"
+                <Card style={{ width: '50rem' }}>
+                    <Row className="justify-content-center p-4 fs-4">Hello There!</Row>
+                    <Container className="ml-4">
+                        <Form className="justify-content-center" noValidate onSubmit={handleSubmit}>
+                            <Row className="mb-2">
+                                <Col>
+                                    <TextField
+                                        label="Full Name"
+                                        type="search"
+                                        //variant="standard"
+                                        fullWidth
                                     />
-                                    {/* <InputGroup.Text id="inputGroupPrepend">@{schoolMailTemp}</InputGroup.Text> */}
-                                    <Form.Control.Feedback type="invalid">
-                                        Please provide a valid Student Email
-                                    </Form.Control.Feedback>
-                                </InputGroup>
-                                <Form.Text className="text-muted">
-                                    We'll never share your email with anyone else.
-                                </Form.Text>
-                            </Form.Group>
-                        </Row>
-                        <Row className="mb-2">
-                            <Form.Group as={Col} md="6" controlId="Password">
-                                <Form.Label className="fs-5">Password</Form.Label>
-                                <Form.Control
-                                    disabled
-                                    type="password"
-                                    placeholder="Password"
-                                    required
-                                    onChange={handleInput}
-                                    autoComplete="new-password"
-                                />
-                                <Form.Control.Feedback type="invalid">
-                                    Please provide a valid Password.
-                                </Form.Control.Feedback>
-                            </Form.Group>
-                            <Form.Group as={Col} md="6" controlId="confirmPassword">
-                                <Form.Label className="fs-5">Confirm Password</Form.Label>
-                                <Form.Control
-                                    disabled
-                                    type="password"
-                                    placeholder="Confirm Password"
-                                    required
-                                    onChange={e => setConfirmPassword(e.target.value)}
-                                    autoComplete="new-password"
-                                />
-                            </Form.Group>
-                        </Row>
-                        <Row className="mb-2">
-                            <Form.Group as={Col} md="4" controlId="CGPA">
-                                <Form.Label className="fs-5">CGPA</Form.Label>
-                                <Form.Control
-                                    disabled
-                                    type="text"
-                                    placeholder="e.g.3.66"
-                                    required
-                                    onChange={handleInput}
-                                />
-                                <Form.Control.Feedback type="invalid">
-                                    Please Enter a CGPA.
-                                </Form.Control.Feedback>
-                            </Form.Group>
-                            <Form.Group as={Col} md="8" className="position-relative mb-3">
-                                <Form.Label className="fs-5">Image Prove</Form.Label>
-                                <Form.Control
-                                    disabled
-                                    id="file"
-                                    type="file"
-                                    required
-                                    name="file"
-                                    accept="image/*"//accept image type file only
-                                    onChange={(e) => {setFile(e.target.files[0]);}}
-                                />
-                                <Form.Control.Feedback type="invalid">
-                                    Please send us a copy of your transcript to confirm your GPA
-                                </Form.Control.Feedback>
-                            </Form.Group>
-                        </Row>
-                    </Form>
-                    {error && <Alert variant="danger">Error! {error}</Alert>}
-                </Container>
-            </Card>
+                                </Col>
+                                <Col>
+                                    <TextField
+                                        label="User Name"
+                                        type="search"
+                                        //variant="standard"
+                                        fullWidth
+                                    />
+                                </Col>
+                            </Row>
+                            <Row className="mb-2">
+                                <Col>
+                                    <TextField
+                                        label="Email"
+                                        type="search"
+                                        //variant="standard"
+                                        fullWidth
+                                    />
+                                </Col>
+                            </Row>
+                            <Row className="mb-2">
+                                <Col>
+                                    <TextField
+                                        label="Password"
+                                        type="search"
+                                        //variant="standard"
+                                        fullWidth
+                                    />
+                                </Col>
+                                <Col>
+                                    <TextField
+                                        label="Confirm Password"
+                                        type="search"
+                                        //variant="standard"
+                                        fullWidth
+                                    />
+                                </Col>
+                            </Row>
+                            <Row className="mb-2">
+
+                            </Row>
+                        </Form>
+                        {error && <Alert variant="danger">Error! {error}</Alert>}
+                    </Container>
+                </Card>
             </Container>
         </Container>
     )
